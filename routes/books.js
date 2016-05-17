6f8309dd2480f3ada1aab800fa508417c0f21893
HongSeokyou <syhong0714@gmail.com>
2016-05-17 15:01:56 +0900

    post - api/1/books finished. But the muster(image upload module is remained).

--- routes/books.js
+++ routes/books.js
@@ -8,7 +8,25 @@ var db = require('../models/db');
 require('../models/bookmodel');
 var BookModel = db.model('Book');
 
-/* GET home page. */
+// POST 이미지 업로드
+router.post('/', function(req, res, next) {
+  var data = new BookModel({
+    user_id   : req.body.user_id,
+    username  : req.body.username,
+    user_type : req.body.user_type,
+    book_desc : req.body.book_desc
+    // 다른 것들도 여기에 추가.
+    });
+  data.save(function(err, docs) {
+    if (err) {
+      console.log('err: ', err)
+    } else {
+      res.send('ok');
+    }
+  });
+});
+
+// GET 뉴스피드
 router.get('/', function(req, res, next) {
   var obj = {
     book_id : 1,
